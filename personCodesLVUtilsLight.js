73b36e60e51d1833824761ee8cfada8ff19874ca
stuzeneger <60490497+stuzeneger@users.noreply.github.com>
2020-11-13 00:26:00 +0200

    Update personCodesLVUtilsLight.js

--- personCodesLVUtilsLight.js
+++ personCodesLVUtilsLight.js
@@ -1,34 +1,28 @@
-var PersonCodesUtils = function (personCode) {
+var PersonCodesUtils = function (personCode) {    
     this.validLVPersonCode = function (personCode) {
-        if (personCode.length !== 12
-            && personCode.length !== 11
-            && personCode.charAt(6) != '-') {
+        if (false === new RegExp('^([0-2][0-9]|(3)[0-1])(((0)[0-9])|((1)[0-2]))\\d{2}(\\-?)\\d{5}$').test(personCode)) {
             return false;
         } else {
-            if (personCode.charAt(6) === '-') {
-                personCode = personCode.substr(0, 6) + personCode.substr(7, personCode.length);
-            }
-            if (personCode.match(/^[0-9]+$/) === null) {
+            personCode.charAt(6) === '-' && personCode = personCode.slice(0, 6) + personCode.slice(-5);
+            let day = personCode.slice(0, 2),
+                month = personCode.slice(2, 2),
+                year = personCode.slice(4, 2),
+                date = new Date();
+            year = (year.substr(0, 1) === '0' ? '20' : '19') + year;
+            date.setFullYear(year, month, day);
+            if (false === (date.getFullYear() == year
+            && date.getMonth() == month
+            && date.getDate() == day ? true : false)) {
                 return false;
-            } else {
-                let day = personCode.substr(0, 2),
-                    month = personCode.substr(2, 2),
-                    year = personCode.substr(4, 2),
-                    date = new Date();
-                year = (year.substr(0, 1) === '0' ? '20' : '19') + year;
-                date.setFullYear(year, month, day);
-                if (false === (date.getFullYear() == year && date.getMonth() == month && date.getDate() == day ? true : false)) {
-                    return false;
-                } else if (!(1101 - [1, 6, 3, 7, 9, 10, 5, 8, 4, 2].reduce(function (sum, value, index) {
-                    return sum += value * personCode[index]
-                }, 0)) % 11 == parseInt(personCode[10]) ? true : false) {
-                    return false;
-                }
+            } else if (!(1101 - [1, 6, 3, 7, 9, 10, 5, 8, 4, 2].reduce(function (sum, value, index) {
+                return sum += value * personCode[index]
+            }, 0)) % 11 == parseInt(personCode[10]) ? true : false) {
+                return false;
             }
             return true;
         }
     }
-    
+
     this.outputPersonCode = function (personCode, isLV = false) {
         isLV = !isLV ? validLVPersonCode(personCode) : isLV;
         if (isLV && personCode.length === 11) {
